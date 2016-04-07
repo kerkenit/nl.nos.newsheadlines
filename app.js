72a99c1333946234f447c2018b61544bcd5b71db
ChristianVersloot <mail@christianversloot.nl>
2016-04-07 13:25:53 +0200

    First version of some functionality related to the trigger block

--- app.js
+++ app.js
@@ -22,6 +22,22 @@ exports.init = function () {
 		}
 	});
 
+	// Homey checks for the flow trigger
+	// whether one of the headlines contains certain words
+	Homey.manager('flow').on('trigger.newsheadline_contains', function( callback, args ){
+		console.log(args);
+		var result = false;
+		require('http.min').json('http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=100&q=http://feeds.feedburner.com/euronews/en/news/').then(function (data) {
+			for(var i = 0; i < 8; i++) {
+				if(data.responseData.feed.entries[i].title.indexOf() > -1) {
+					result=true;
+					break;
+				}
+			}
+			callback(null, result);
+		}
+	});
+
 	// Homey checks for the news headlines to be triggered
 	// i.e. through phrases like
 	// What are the news headlines?
