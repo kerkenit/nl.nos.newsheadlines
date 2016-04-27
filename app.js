61bdc898a7041d961ed6b857aa7c2c7982b1ed1a
Kerk en IT <info@kerkenit.nl>
2016-04-27 10:51:47 +0200

    Fixed failure callback
    Callback will return true if data is available.
    If the NOS feed fails, it wil return false

--- app.js
+++ app.js
@@ -13,35 +13,40 @@ exports.init = function () {
 		// and formulate the news headlines
 		Homey.log('News headlines are being downloaded');
 		require('http.min').json('http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=100&q=http://feeds.nos.nl/nosjournaal').then(function (data) {
+			if(data !== undefined && data != null &&
+			data.responseData !== undefined && data.responseData !== null &&
+			data.responseData.feed !== undefined && data.responseData.feed !== null &&
+			data.responseData.feed.entries !== undefined && data.responseData.feed.entries !== null &&
+			data.responseData.feed.entries.length > 0 ) {
+				// Concatenate everything
+				var newsHeadlines = [];
+				var maxNews = Homey.manager('settings').get('numberOfNewsArticles');
+					maxNews = (maxNews > 8 ? 8 : (maxNews < 1 ? 1 : maxNews)); // Minimum of 1 article, maximum of 8 articles (~source limit)
+					newsHeadlines.push('Je recente nieuws berichten.');
 
-			// Concatenate everything
-			var newsHeadlines = [];
-			var maxNews = Homey.manager('settings').get('numberOfNewsArticles');
-				maxNews = (maxNews > 8 ? 8 : (maxNews < 1 ? 1 : maxNews)); // Minimum of 1 article, maximum of 8 articles (~source limit)
-			newsHeadlines.push('Je recente nieuws berichten.');
-
-			for(var i = 0; i < maxNews; i++) {
-				var title = data.responseData.feed.entries[i].title;
-				if (title[title.length-1] === ".") {
-					title = title.slice(0,-1);
+				for(var i = 0; i < maxNews; i++) {
+					var title = data.responseData.feed.entries[i].title;
+					if (title[title.length - 1] === ".") {
+						title = title.slice(0, -1);
+					}
+					var content = data.responseData.feed.entries[i].content;
+					if (content[content.length - 1] === ".") {
+						content = content.slice(0, -1);
+					}
+					newsHeadlines.push(headlineKeywords[i] + '. ' + title + '. ' + content + '........ ');
+					title = null;
+					content = null;
 				}
-				var content = data.responseData.feed.entries[i].content;
-				if (content[content.length-1] === ".") {
-					content = content.slice(0,-1);
+
+				// Spread the word
+				for(var i = 0; i < newsHeadlines.length; i++) {
+					Homey.manager('speech-output').say(__(newsHeadlines[i]));
 				}
-				newsHeadlines.push(headlineKeywords[i] + '. ' + title + '. ' + content + '........ ');
-				title = null;
-				content = null;
+				callback(null, true);
+			} else {
+				callback(null, false);
 			}
-
-			// Spread the word
-			for(var i = 0; i < newsHeadlines.length; i++) {
-				Homey.manager('speech-output').say(__(newsHeadlines[i]));
-			}
-
 		});
-		// Callback
-		callback();
 	});
 
 	// Homey checks for the news headlines to be triggered
@@ -59,32 +64,36 @@ exports.init = function () {
 				// and formulate the news headlines
 				Homey.log('News headlines are being downloaded');
 				require('http.min').json('http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=100&q=http://feeds.nos.nl/nosjournaal').then(function (data) {
+					if(data !== undefined && data != null &&
+					data.responseData !== undefined && data.responseData !== null &&
+					data.responseData.feed !== undefined && data.responseData.feed !== null &&
+					data.responseData.feed.entries !== undefined && data.responseData.feed.entries !== null &&
+					data.responseData.feed.entries.length > 0 ) {
+						// Concatenate everything
+						var newsHeadlines = [];
+						var maxNews = Homey.manager('settings').get('numberOfNewsArticles');
+							maxNews = (maxNews > 8 ? 8 : (maxNews < 1 ? 1 : maxNews)); // Minimum of 1 article, maximum of 8 articles (~source limit)
+							newsHeadlines.push('Je recente nieuws berichten.');
 
-					// Concatenate everything
-					var newsHeadlines = [];
-					var maxNews = Homey.manager('settings').get('numberOfNewsArticles');
-						maxNews = (maxNews > 8 ? 8 : (maxNews < 1 ? 1 : maxNews)); // Minimum of 1 article, maximum of 8 articles (~source limit)
-					newsHeadlines.push('Je recente nieuws berichten.');
-
-					for(var i = 0; i < maxNews; i++) {
-						var title = data.responseData.feed.entries[i].title;
-						if (title[title.length-1] === ".") {
-							title = title.slice(0,-1);
+						for(var i = 0; i < maxNews; i++) {
+							var title = data.responseData.feed.entries[i].title;
+							if (title[title.length-1] === ".") {
+								title = title.slice(0,-1);
+							}
+							var content = data.responseData.feed.entries[i].content;
+							if (content[content.length-1] === ".") {
+								content = content.slice(0,-1);
+							}
+							newsHeadlines.push(headlineKeywords[i] + '. ' + title + '. ' + content + '........ ');
+							title = null;
+							content = null;
 						}
-						var content = data.responseData.feed.entries[i].content;
-						if (content[content.length-1] === ".") {
-							content = content.slice(0,-1);
+
+						// Spread the word
+						for(var i = 0; i < newsHeadlines.length; i++) {
+							Homey.manager('speech-output').say(__(newsHeadlines[i]));
 						}
-						newsHeadlines.push(headlineKeywords[i] + '. ' + title + '. ' + content + '........ ');
-						title = null;
-						content = null;
 					}
-
-					// Spread the word
-					for(var i = 0; i < newsHeadlines.length; i++) {
-						Homey.manager('speech-output').say(__(newsHeadlines[i]));
-					}
-
 				});
 			}
 		});
