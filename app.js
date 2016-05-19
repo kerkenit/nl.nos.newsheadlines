c5782e225aa9b5b0cb6839bd843706fbe0e2c0c8
Kerk en IT <info@kerkenit.nl>
2016-05-19 23:17:37 +0200

    Add string split after 255 characters. Added word replace

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
 "use strict";
 exports.init = function() {
+	var striptags = require('striptags');
+
 	var numberOfNewsArticles = Homey.manager('settings').get('numberOfNewsArticles');
 	if (numberOfNewsArticles === undefined || numberOfNewsArticles === null) {
 		// Set the standard number of news headlines to 20
 		Homey.manager('settings').set('numberOfNewsArticles', 5);
 	}
 	var headlineKeywords = [__('app.numbers.one'), __('app.numbers.two'), __('app.numbers.three'), __('app.numbers.four'), __('app.numbers.five'), __('app.numbers.six'), __('app.numbers.seven'), __('app.numbers.eight'), __('app.numbers.nine'), __('app.numbers.ten'), __('app.numbers.eleven'), __('app.numbers.twelve'), __('app.numbers.thirteen'), __('app.numbers.fourteen'), __('app.numbers.fiveteen'), __('app.numbers.sixteen'), __('app.numbers.seventeen'), __('app.numbers.eightteen'), __('app.numbers.nineteen'), __('app.numbers.twenty')];
+
+	var formatHeadline = function(text) {
+		text = text.replace('"', '');
+		text = text.replace("'", "");
+		text = text.replace("\"", "");
+		text = striptags(text).substr(0, 255);
+		var index = text.lastIndexOf('.');
+		return [text.slice(0, index), text.slice(index + 1)][0]+'.';
+	};
+
+	var replaceContent = function(text) {
+		text = text.replace(__('app.findandreplace.kmph.find'), __('app.findandreplace.kmph.replace'));
+		text = text.replace(__('app.findandreplace.mph.find'), __('app.findandreplace.mph.replace'));
+		text = text.replace(__('app.findandreplace.co2.find'), __('app.findandreplace.co2.replace'));
+		text = text.replace(__('app.findandreplace.rivm.find'), __('app.findandreplace.rivm.replace'));
+		text = text.replace(__('app.findandreplace.om.find'), __('app.findandreplace.om.replace'));
+		text = text.replace(__('app.findandreplace.rdw.find'), __('app.findandreplace.rdw.replace'));
+		text = text.replace(__('app.findandreplace.who.find'), __('app.findandreplace.who.replace'));
+		text = text.replace(__('app.findandreplace.bbc.find'), __('app.findandreplace.bbc.replace'));
+		text = text.replace(__('app.findandreplace.youp.find'), __('app.findandreplace.youp.replace'));
+		return text;
+	};
 	// Homey checks if it should read the news
 	Homey.manager('flow').on('action.readNews', function(callback) {
 		// Read the news
 		// Download news headlines in JSON format,
 		// and formulate the news headlines
-		Homey.log('News headlines are being downloaded');
-		var striptags = require('striptags');
-		require('http.min').json('http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=100&q=http://feeds.nos.nl/nosjournaal').then(function(data) {
-			if (data !== undefined && data != null && data.responseData !== undefined && data.responseData !== null && data.responseData.feed !== undefined && data.responseData.feed !== null && data.responseData.feed.entries !== undefined && data.responseData.feed.entries !== null && data.responseData.feed.entries.length > 0) {
+		Homey.log('News headlines are being downloaded');		require('http.min').json('http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=100&q=http://feeds.nos.nl/nosjournaal').then(function(data) {
+			if (data !== undefined && data !== null && data.responseData !== undefined && data.responseData !== null && data.responseData.feed !== undefined && data.responseData.feed !== null && data.responseData.feed.entries !== undefined && data.responseData.feed.entries !== null && data.responseData.feed.entries.length > 0) {
 				// Concatenate everything
 				var newsHeadlines = [];
 				var Headlines = [];
@@ -22,26 +44,21 @@ exports.init = function() {
 				maxNews = (maxNews > 20 ? 20 : (maxNews < 1 ? 1 : maxNews)); // Minimum of 1 article, maximum of 20 articles (~source limit)
 				newsHeadlines.push(__('app.speechPrefix'));
 				for (var i = 0; i < maxNews; i++) {
-					var title = data.responseData.feed.entries[i].title;
-					if (title[title.length - 1] === ".") {
-						title = title.slice(0, -1);
+					if(data.responseData.feed.entries[i].title !== undefined) {
+						var title = replaceContent(data.responseData.feed.entries[i].title);
+						var content = replaceContent(data.responseData.feed.entries[i].content);
+						if (title.length > 0 && content.length > 0) {
+							newsHeadlines.push(formatHeadline(headlineKeywords[i] + '. ' + title + '. ' + content));
+						}
+						title = null;
+						content = null;
 					}
-					var content = data.responseData.feed.entries[i].content;
-					if (content[content.length - 1] === ".") {
-						content = content.slice(0, -1);
-					}
-					if (title.length > 0 && content.length > 0) {
-						newsHeadlines.push(headlineKeywords[i] + '. ' + title + '. ' + content + '........ ');
-					}
-					title = null;
-					content = null;
 				}
 				// Spread the word
-				for (var i = 0; i < newsHeadlines.length; i++) {
-					Homey.log(newsHeadlines[i] + "    ");
-					Headlines[i]=striptags(newsHeadlines[i]);
-					//Homey.log(Headlines[i] + "    ");
-					Homey.manager('speech-output').say(__(Headlines[i].substr(0,255)));
+				for (var j = 0; j < newsHeadlines.length; j++) {
+					//Homey.log(newsHeadlines[j].length);
+					//Homey.log(newsHeadlines[j]);
+					Homey.manager('speech-output').say(__(newsHeadlines[j]));
 				}
 				callback(null, true);
 			} else {
@@ -65,33 +82,26 @@ exports.init = function() {
 				Homey.log('News headlines are being downloaded');
 				var striptags = require('striptags');
 				require('http.min').json('http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=100&q=http://feeds.nos.nl/nosjournaal').then(function(data) {
-					if (data !== undefined && data != null && data.responseData !== undefined && data.responseData !== null && data.responseData.feed !== undefined && data.responseData.feed !== null && data.responseData.feed.entries !== undefined && data.responseData.feed.entries !== null && data.responseData.feed.entries.length > 0) {
+					if (data !== undefined && data !== null && data.responseData !== undefined && data.responseData !== null && data.responseData.feed !== undefined && data.responseData.feed !== null && data.responseData.feed.entries !== undefined && data.responseData.feed.entries !== null && data.responseData.feed.entries.length > 0) {
 						// Concatenate everything
 						var newsHeadlines = [];
 						var maxNews = Homey.manager('settings').get('numberOfNewsArticles');
 						maxNews = (maxNews > 20 ? 20 : (maxNews < 1 ? 1 : maxNews)); // Minimum of 1 article, maximum of 20 articles (~source limit)
 						newsHeadlines.push(__('app.speechPrefix'));
 						for (var i = 0; i < maxNews; i++) {
-							var title = data.responseData.feed.entries[i].title;
-							if (title[title.length - 1] === ".") {
-								title = title.slice(0, -1);
-							}
-							var content = data.responseData.feed.entries[i].content;
-							if (content[content.length - 1] === ".") {
-								content = content.slice(0, -1);
-							}
-							if (title.length > 0 && content.length > 0) {
-								newsHeadlines.push(headlineKeywords[i] + '. ' + title + '. ' + content + '........ ');
-							}
-							title = null;
-							content = null;
+							var title = replaceContent(data.responseData.feed.entries[i].title);
+						var content = replaceContent(data.responseData.feed.entries[i].content);
+						if (title.length > 0 && content.length > 0) {
+							newsHeadlines.push(formatHeadline(headlineKeywords[i] + '. ' + title + '. ' + content));
+						}
+						title = null;
+						content = null;
 						}
 						// Spread the word
-						for (var i = 0; i < newsHeadlines.length; i++) {
-					Homey.log(newsHeadlines[i] + "    ");
-					Headlines[i]=striptags(newsHeadlines[i]);
-					//Homey.log(Headlines[i] + "    ");
-					Homey.manager('speech-output').say(__(Headlines[i].substr(0,255)));
+						for (var j = 0; j < newsHeadlines.length; j++) {
+							//Homey.log(newsHeadlines[j].length);
+							//Homey.log(newsHeadlines[j]);
+							Homey.manager('speech-output').say(__(newsHeadlines[j]));
 						}
 					}
 				});
